771757d764b838bd96ca64b8b0ddee6aa375e9f8
EneroMangal4321 <enero.mangal@student.rocva.nl>
2020-02-24 16:00:11 +0100

    update

--- oefen_api/text-api.py
+++ oefen_api/text-api.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route("/", methods = ["GET"])
 def schrijf_json():
-    x = open("text.json")
+    x = '{ "name": "Enero", "age": 18, "friends": ["Anass", "Aman", "Ayoub"] }' 
     text = json.loads(x)
-    return text
+    return text["friends"]
 
 if __name__ == "__main__":
     app.run()
