d929eaa0df78c0bb458cb54bf2f68a9892944bae
EneroMangal4321 <enero.mangal@student.rocva.nl>
2020-02-24 11:17:25 +0100

    text api update

--- oefen_api/text-api.py
+++ oefen_api/text-api.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, json
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Flask"
+
+def schrijf_json():
+    text1 = (request.json["key"])
+    text2 = (request.json["value"])
+
+if __name__ == "__main__":
+    app.run(debug=True)
