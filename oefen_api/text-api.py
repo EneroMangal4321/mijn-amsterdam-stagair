9f211063e836d083474365dfb50184f30618ea33
EneroMangal4321 <enero.mangal@student.rocva.nl>
2020-02-24 17:10:03 +0100

    Update text-api.py

--- oefen_api/text-api.py
+++ oefen_api/text-api.py
@@ -4,15 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def schrijf_json():
-    '''x = '{ "name": "Enero", "age": 18, "friends": ["Anass", "Aman", "Ayoub"] }' 
-    text = json.loads(x)'''
-
     with open("text.json") as myfile:
         data = myfile.read()
 
     obj = json.loads(data)
 
-    return obj["name"]
+    return str(obj)
 
 if __name__ == "__main__":
     app.run(debug=True)
