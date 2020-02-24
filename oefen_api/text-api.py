516fbdffd132f9dd2dcbfd6a1986c1ab5c443494
EneroMangal4321 <enero.mangal@student.rocva.nl>
2020-02-24 16:31:50 +0100

    yes

--- oefen_api/text-api.py
+++ oefen_api/text-api.py
@@ -2,11 +2,11 @@ from flask import Flask, json, request
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET"])
+@app.route("/", method="GET")
 def schrijf_json():
     x = '{ "name": "Enero", "age": 18, "friends": ["Anass", "Aman", "Ayoub"] }' 
     text = json.loads(x)
-    return text["friends"]
+    return text["name"]
 
 if __name__ == "__main__":
     app.run()
