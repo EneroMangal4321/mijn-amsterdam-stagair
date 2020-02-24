b2838b4f714d0dc7ebc02e948e04ffd1b3e14c73
EneroMangal4321 <enero.mangal@student.rocva.nl>
2020-02-24 17:04:37 +0100

    delete shitty env

--- oefen_api/text-api.py
+++ oefen_api/text-api.py
@@ -2,11 +2,17 @@ from flask import Flask, json, request
 
 app = Flask(__name__)
 
-@app.route("/", method="GET")
+@app.route("/")
 def schrijf_json():
-    x = '{ "name": "Enero", "age": 18, "friends": ["Anass", "Aman", "Ayoub"] }' 
-    text = json.loads(x)
-    return text["name"]
+    '''x = '{ "name": "Enero", "age": 18, "friends": ["Anass", "Aman", "Ayoub"] }' 
+    text = json.loads(x)'''
+
+    with open("text.json") as myfile:
+        data = myfile.read()
+
+    obj = json.loads(data)
+
+    return obj["name"]
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
