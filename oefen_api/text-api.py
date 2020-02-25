497201294ca634bc039b18a7b50d11efdccf2c24
EneroMangal4321 <enero.mangal@student.rocva.nl>
2020-02-25 15:34:11 +0100

    Update text-api.py

--- oefen_api/text-api.py
+++ oefen_api/text-api.py
@@ -4,12 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def json_formatting():
+    data = None
     with open("text.json") as myfile:
         data = myfile.read()
 
     obj = json.loads(data)
     text = json.dumps(obj, sort_keys=False)
-    return str(text["name"])
+    return text
 
 if __name__ == "__main__":
     app.run(debug=True)
