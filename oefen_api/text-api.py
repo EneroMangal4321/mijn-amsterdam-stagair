9cf1d895dddd4cc7ea55bf25d160ff5a1311e3ae
EneroMangal4321 <enero.mangal@student.rocva.nl>
2020-02-26 10:06:53 +0100

    Update text-api.py

--- oefen_api/text-api.py
+++ oefen_api/text-api.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 def json_formatting():
     data = None
     with open("text.json") as myfile:
-        data = myfile.read()
-
+        try:
+            data = myfile.read()
+        except KeyError:
+            return None
     obj = json.loads(data)
-    text = json.dumps(obj, sort_keys=False)
-    return text
+    #hier is ruimte om iets met de inhoud van text.json te doen
+    #text = json.dumps(obj, sort_keys=False)
+    return json.dumps(obj["name"], sort_keys=False)
 
 if __name__ == "__main__":
     app.run(debug=True)
