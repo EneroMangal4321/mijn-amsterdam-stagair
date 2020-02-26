f9af4eed523a6bee61ab14d13c2373f9592df238
EneroMangal4321 <enero.mangal@student.rocva.nl>
2020-02-26 10:35:06 +0100

    Update text-api.py

--- oefen_api/text-api.py
+++ oefen_api/text-api.py
@@ -7,13 +7,13 @@ def json_formatting():
     data = None
     with open("text.json") as myfile:
         try:
-            data = myfile.read()
+            data = json.load(myfile)
         except KeyError:
             return None
-    obj = json.loads(data)
     #hier is ruimte om iets met de inhoud van text.json te doen
     #text = json.dumps(obj, sort_keys=False)
-    return json.dumps(obj["name"], sort_keys=False)
+    #return json.dumps(data, sort_keys=False)
+    return json.dumps(data, sort_keys=False)
 
 if __name__ == "__main__":
     app.run(debug=True)
