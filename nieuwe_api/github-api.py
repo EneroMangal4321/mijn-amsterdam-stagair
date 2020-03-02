f36873d412d0b5ef37aa9016e84222ddcd73391c
EneroMangal4321 <enero.mangal@student.rocva.nl>
2020-03-02 09:47:36 +0100

    Update github-api.py
    Is dit wat het ongeveer moet worden?

--- nieuwe_api/github-api.py
+++ nieuwe_api/github-api.py
@@ -7,20 +7,19 @@ app = Flask(__name__)
 @app.route("/")
 def get_repo():
     repos_url = "https://api.github.com/users/EneroMangal4321/repos"
-    response = requests.get(repos_url)
-    response_list = response.json()
-    for x in response_list:
-        print(x["name"])
-    return {"repos": response.json()}
+    repos_response = requests.get(repos_url)
+    repos_response_list = repos_response.json()
+    for x in repos_response_list:
+        print("{")
+        print("repo:"  + json.dumps(x["name"]))
+        commit_url = "https://api.github.com/repos/EneroMangal4321/" + x["name"] + "/commits"
+        commit_response = requests.get(commit_url)
+        commit_response_list = commit_response.json()
+        eerste_item = commit_response_list[0]['commit']['message']
+        print("commit:" + json.dumps(eerste_item))
+        print("}")
 
-@app.route("/commits")
-def get_commits():
-    commit_url = "https://api.github.com/repos/EneroMangal4321/mijn-amsterdam-stagair/commits"
-    commit_response = requests.get(commit_url)
-    commit_response_list = commit_response.json()
-    for x in commit_response_list:
-        print(x)
-    return {"commits": commit_response.json()}
+    return {"repos": repos_response.json()} 
    
 
 if __name__ == "__main__":
