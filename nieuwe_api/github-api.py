1c0133861470c0dae5f400d79ceed72c5b337671
EneroMangal4321 <enero.mangal@student.rocva.nl>
2020-03-02 16:12:55 +0100

    Update github-api.py

--- nieuwe_api/github-api.py
+++ nieuwe_api/github-api.py
@@ -1,31 +1,35 @@
-from flask import Flask, json, jsonify
+from flask import Flask, json, jsonify, Response, request
 import requests
 from pprint import pprint
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/username")
 def get_repo():
-    repos_url = "https://api.github.com/users/EneroMangal4321/repos"
+    username = request.args.get("username")
+    repos_url = "https://api.github.com/users/%s/repos" % (username)
     repos_response = requests.get(repos_url)
     repos_response_list = repos_response.json()
-
     item_list = []
 
-    for x in repos_response_list:
-        commit_url = "https://api.github.com/repos/EneroMangal4321/" + x["name"] + "/commits"
+    for repos_info_response in repos_response_list:
+        try:
+            commit_url = "https://api.github.com/repos/%s/%s/commits" % (username, repos_info_response["name"])
+        except:
+            return "Je hebt een verkeerde gebruikersnaam ingevuld. Vul de goede gebruikersnaam in."
         commit_response = requests.get(commit_url)
+        print("5")
         commit_response_list = commit_response.json()
         eerste_item = commit_response_list[0]['commit']['message']
 
-
         item = {
-            "repo": x["name"],
-            "commit": eerste_item
+            "repository_name": repos_info_response["name"],
+            "last_submitted_commit": eerste_item
         }
         item_list.append(item)
-    json.dumps(item_list)
-    return item_list #jsonify(item_list)
+    
+    item_list_json = json.dumps(item_list, sort_keys=False)
+    return Response(item_list_json, mimetype='application/json') #jsonify(item_list)
    
 
 if __name__ == "__main__":
